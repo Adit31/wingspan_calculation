ea912ba4fba593bda5f0cddf271f0916a8833b4a
Adit <aditgoyal2024@u.northwestern.edu>
2023-08-17 13:31:51 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -30,62 +30,69 @@ def draw_pose_landmarks_on_image(rgb_image, detection_result):
   return annotated_image
 
 def calculate_palm(image_path_palm):
-    base_options = python.BaseOptions(model_asset_path=hand_landmark_model_path)
-    options = vision.HandLandmarkerOptions(base_options=base_options,
-                                        num_hands=2)
-    detector = vision.HandLandmarker.create_from_options(options)
-
-    image = mp.Image.create_from_file(image_path_palm)
-    height, width = image.height, image.width
-
-    detection_result = detector.detect(image)
-
-    middle_finger_tip = detection_result.hand_landmarks[0][12]
-    wrist = detection_result.hand_landmarks[0][0]
-
-    middle_finger_tip_x = middle_finger_tip.x * width
-    middle_finger_tip_y = middle_finger_tip.y * height
-
-    wrist_x = wrist.x * width
-    wrist_y  = wrist.y * height
-    palm_size_pixels = np.sqrt((middle_finger_tip_x - wrist_x)**2 + (middle_finger_tip_y - wrist_y)**2)
-
-    return palm_size_pixels
+    try:
+        base_options = python.BaseOptions(model_asset_path=hand_landmark_model_path)
+        options = vision.HandLandmarkerOptions(base_options=base_options,
+                                            num_hands=2)
+        detector = vision.HandLandmarker.create_from_options(options)
+    
+        image = mp.Image.create_from_file(image_path_palm)
+        height, width = image.height, image.width
+    
+        detection_result = detector.detect(image)
+    
+        middle_finger_tip = detection_result.hand_landmarks[0][12]
+        wrist = detection_result.hand_landmarks[0][0]
+    
+        middle_finger_tip_x = middle_finger_tip.x * width
+        middle_finger_tip_y = middle_finger_tip.y * height
+    
+        wrist_x = wrist.x * width
+        wrist_y  = wrist.y * height
+        palm_size_pixels = np.sqrt((middle_finger_tip_x - wrist_x)**2 + (middle_finger_tip_y - wrist_y)**2)
+    
+        return palm_size_pixels
+    except IndexError:
+        st.write("The application requires at least one arm to be visible in the frame")
+        return 0
 
 def calculate_wingspan(image_path_wingspan, height_meters, height_pixels):
-    base_options = python.BaseOptions(model_asset_path=pose_landmark_model_path)
-    options = vision.PoseLandmarkerOptions(
-        base_options=base_options,
-        output_segmentation_masks=True)
-    detector = vision.PoseLandmarker.create_from_options(options)
-
-    image = mp.Image.create_from_file(image_path_wingspan)
-    height, width = image.height, image.width
-
-    detection_result = detector.detect(image)
-
-    annotated_image = draw_pose_landmarks_on_image(image.numpy_view(), detection_result)
-
-    left_wrist = detection_result.pose_landmarks[0][15]
-    right_wrist = detection_result.pose_landmarks[0][16]
-
-    x_left_wrist = left_wrist.x *width
-    y_left_wrist = left_wrist.y *height
-
-    x_right_wrist = right_wrist.x *width
-    y_right_wrist = right_wrist.y *height
-
-    wingspan_pixels = np.sqrt((x_right_wrist - x_left_wrist)**2 + (y_right_wrist - y_left_wrist)**2 )
-
-    palm_pixels = calculate_palm(image_path_wingspan)
-
-    total_wingspan_pixels = wingspan_pixels + 2*palm_pixels
-
-    pixels_to_meters = height_meters / height_pixels
-    total_wingspan_meters = total_wingspan_pixels * pixels_to_meters
-
-    return total_wingspan_meters, annotated_image
-
+    try:
+        base_options = python.BaseOptions(model_asset_path=pose_landmark_model_path)
+        options = vision.PoseLandmarkerOptions(
+            base_options=base_options,
+            output_segmentation_masks=True)
+        detector = vision.PoseLandmarker.create_from_options(options)
+    
+        image = mp.Image.create_from_file(image_path_wingspan)
+        height, width = image.height, image.width
+    
+        detection_result = detector.detect(image)
+    
+        annotated_image = draw_pose_landmarks_on_image(image.numpy_view(), detection_result)
+    
+        left_wrist = detection_result.pose_landmarks[0][15]
+        right_wrist = detection_result.pose_landmarks[0][16]
+    
+        x_left_wrist = left_wrist.x *width
+        y_left_wrist = left_wrist.y *height
+    
+        x_right_wrist = right_wrist.x *width
+        y_right_wrist = right_wrist.y *height
+    
+        wingspan_pixels = np.sqrt((x_right_wrist - x_left_wrist)**2 + (y_right_wrist - y_left_wrist)**2 )
+    
+        palm_pixels = calculate_palm(image_path_wingspan)
+    
+        total_wingspan_pixels = wingspan_pixels + 2*palm_pixels
+    
+        pixels_to_meters = height_meters / height_pixels
+        total_wingspan_meters = total_wingspan_pixels * pixels_to_meters
+    
+        return total_wingspan_meters, annotated_image
+    except IndexError:
+        st.write("The application requires at least one arm to be visible in the frame")
+        return 0, image_path_wingspan
 
 st.title("Calculate Wingspan")
 
