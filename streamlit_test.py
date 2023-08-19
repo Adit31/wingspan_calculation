b8243367bac6c88b2feed6d1b76a92b31707d609
Adit <aditgoyal2024@u.northwestern.edu>
2023-08-19 17:11:07 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -55,7 +55,7 @@ def calculate_palm(image_path_palm):
         st.write("The application requires at least one arm to be visible in the frame")
         return 0
 
-def calculate_wingspan(image_path_wingspan, length_inches, length_pixels):
+def calculate_wingspan(image_path_wingspan, length_inches):
     try:
         base_options = python.BaseOptions(model_asset_path=pose_landmark_model_path)
         options = vision.PoseLandmarkerOptions(
@@ -97,11 +97,11 @@ def calculate_wingspan(image_path_wingspan, length_inches, length_pixels):
 
         total_height_pixels = abs(y_eye - y_heel) + forehead
     
-        pixels_to_inches = length_inches / length_pixels
+        pixels_to_inches = length_inches / total_height_pixels
         total_wingspan_inches = total_wingspan_pixels * pixels_to_inches
-        total_height_inches = total_height_pixels * pixels_to_inches
+        # total_height_inches = total_height_pixels * pixels_to_inches
     
-        return total_wingspan_inches, total_height_inches, annotated_image
+        return total_wingspan_inches, annotated_image
     except IndexError:
         st.write("The application requires at least one arm to be visible in the frame")
         return 0, 0, image_path_wingspan
@@ -114,10 +114,10 @@ if uploaded_file is not None:
         temp_file.write(uploaded_file.read())
         temp_path = temp_file.name
     length_inches = st.number_input("Enter the static height in inches:", value=1.0)
-    length_pixels = st.number_input("Enter the static height in pixels:", value=2.5)
+    # length_pixels = st.number_input("Enter the static height in pixels:", value=2.5)
 
-    wingspan, height, annotated_image = calculate_wingspan(temp_path, length_inches, length_pixels)
+    wingspan, annotated_image = calculate_wingspan(temp_path, length_inches)
     
     st.image(annotated_image, caption="Annotated Image", use_column_width=True)
     st.write("Wingspan is:", wingspan, "inches")
-    st.write("Height is:", height, "inches")
+    # st.write("Height is:", height, "inches")
